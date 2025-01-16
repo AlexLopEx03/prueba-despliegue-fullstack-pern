58c73f44a048bb08bf70505ea1de1c464b7ac940
AlexLopEx03 <alejandrolopezexposito2003@gmail.com>
2025-01-16 02:29:11 +0000

    Añadido cors

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,12 +1,15 @@
 import express from 'express'
-
+import cors from 'cors'
 const app = express()
+app.use(cors({
+    origin: 'https://prueba-despliegue-fullstack-pern.onrender.com'
+}))
 app.use(express.json())
 app.get('/prueba/get', (req, res) => {
     res.send('Respuesta de prueba/get')
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Port: ${PORT}`)
 })
