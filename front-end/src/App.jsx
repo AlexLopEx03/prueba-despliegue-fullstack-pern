0a4f4634767b9afcde86ae21881c3c418c66ee6f
AlexLopEx03 <AlexLopEx03>
2025-01-24 18:40:44 +0000

    Epa

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,7 @@
 import './styles/App.css'
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
+import DataComponent from './components/DataComponent'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <HeaderComponent/>
       <main>
         <h2>Esto es del App.jsx</h2>
+        <DataComponent/>
       </main>
       <FooterComponent/>
     </body>
---
+++ front-end/src/components/DataComponent.jsx
@@ -0,0 +1,11 @@
+import '../styles/components/DataComponent.css'
+
+function DataComponent() {
+  return (
+    <div className="data-component">
+        <h1>Data component</h1>
+    </div>
+  )
+}
+
+export default DataComponent
